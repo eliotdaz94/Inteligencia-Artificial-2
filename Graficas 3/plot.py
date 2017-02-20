8e2e572e28643d39417638cf0add8e788bc9143e
Erick <ericksilvarodriguez@gmail.com>
2017-02-20 10:36:34 -0400

    Mas Graficas

--- Graficas 3/plot.py
+++ Graficas 3/plot.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 
 def combinar(lr):
     for x in lr:
-        c01 = pandas.read_table("x08_{}.txt".format(x),header = 0 , delimiter = " ")
+        c01 = pandas.read_table("train01.txt",header = 0 , delimiter = " ")
         c01 = c01.dropna()
         plt.plot(c01["Iteracion"], c01["FuncionCosto"], label = "{}".format(x))
 
-    a = plt.gca() 
+    a = plt.gca()   
     a.set_ylabel("Función de Costo")
     a.set_xlabel("Iteraciones")   
-    a.set_ylim([0,0.2])
-    a.set_xlim([0,20])
-    a.set_title("Curvas de Convergencia para Datos sobre Mortalidad con Tasas de Aprendizaje = 0.1.",fontsize = 10, y = 1.05)
+    #a.set_ylim([0,0.2])
+    #a.set_xlim([0,20])
+    a.set_title("Curvas de Convergencia para Datos sobre precios de casa con tasa de aprendizaje 0.1.",fontsize = 10, y = 1.05)
     plt.tight_layout()
     plt.legend()
-    plt.savefig("todos3.png")
+    plt.savefig("todos2.png")
 
 arreglo = [0.1]
 combinar(arreglo)
