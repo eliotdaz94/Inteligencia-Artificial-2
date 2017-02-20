00edf811ffb1b6ea3597c45d36d4e5ab20b8a6e1
Gabriel Gimenez <12-11006@usb.ve>
2017-02-19 23:38:19 -0800

    Todas las graficas

--- Graficas 1/plot.py
+++ Graficas 1/plot.py
@@ -1,6 +1,7 @@
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
+INGORAR_LINEAS = 33
 
 def plotear(lr):
     string = "Curva de Convergencia para Datos sobre Peso con Tasa de Aprendizaje ="
@@ -12,5 +13,33 @@ def plotear(lr):
     plt.tight_layout()
     plt.savefig("{}.png".format(lr))
 
+def leer_input(fichero):
+    resultado = []
+    with open(fichero,"r") as file:
+        for line in file.readlines()[INGORAR_LINEAS:-1]:
+            columns = map(float,line.strip().split()[1:])
+            resultado.append(list(columns))
+    return resultado
+
+def generar_df(in_matrix):
+    df = pandas.DataFrame(in_matrix)
+    df.columns = ["Peso del Cerebro", "Peso Corporal"]
+    return df
+
+def normalizar(data_frame):
+    return (data_frame - data_frame.mean())/data_frame.std()
+    
+tabla = generar_df(leer_input("x01.txt"))
+tabla = normalizar(tabla)
+tabla.plot.scatter(0,1)
+t = plt.gca()
+t.set_title("Peso Corporal en función del Peso del Cerebro")
+
+pesos = [8.46415e-006,0.934081]
+pf = [-0.4,7]
+r = [[1,-0.4],[1,7]]
+a = [np.dot(a,pesos) for a in r]
+plt.plot(pf,a,'r')
+plt.savefig("Regresion.png")
 for x in [0.1,1,2]:
     plotear(x)
